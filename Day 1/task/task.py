4a7719d6c3641a36848269e880b379da585b819a
solomon-asmr <selemonasmr7@gmail.com>
2025-08-22 18:31:49 +0300

    day 1 print(), input(),and variables

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,4 @@
-# Write your code below this line 👇
+print("Wellcome to the Band Name Generator.")
+place=input("What's the name of the city you grew up in? \n")
+petName= input("What's your pet's name? \n")
+print("Your band name could be "+ place + " " + petName)
