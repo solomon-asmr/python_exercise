6fe2741360021b13aa676d27ba7d39ce66db5c52
solomon-asmr <selemonasmr7@gmail.com>
2025-08-29 18:58:00 +0300

    Day 9 bidding system

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -2,5 +2,29 @@
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
+from sys import flags
+import os
+import art
+import time
+
+print(art.logo)
+biders_book = {}
+flag=True
+while flag:
+    name= input("What is your name?: ")
+    bid = int(input("What's your bid?: "))
+    biders_book[name]=bid
+    choose = input("Are there any other bidders? Type 'yes' or 'no' ").lower()
+    os.system('cls' if os.name == 'nt' else 'clear')
+    if choose=='no':
+        flag=False
+        maxi=0
+        name_max=""
+        for bidder in biders_book:
+            if biders_book[bidder]>maxi:
+                maxi=biders_book[bidder]
+                name_max=bidder
+        print(f"The winner is {name_max} with a bid of ${maxi}")
+
 
 
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,24 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."
+}
+student_scores={
+    "Harry": 90,
+    "Ron": 78,
+    "hermione": 95,
+    "Draco": 75,
+    "Neville": 60
+}
+student_grades={}
+def scores_to_grades():
+    for name in student_scores:
+        if 91<=student_scores[name] <=100:
+            student_grades[name]="Outstanding"
+        elif 81<=student_scores[name]<=90:
+            student_grades[name]="Exceeds Expectations"
+        elif 71<=student_scores[name]<=80:
+            student_grades[name]="Acceptable"
+        elif student_scores[name]<=70:
+            student_grades[name]="Fail"
+    print(student_grades)
+scores_to_grades()
