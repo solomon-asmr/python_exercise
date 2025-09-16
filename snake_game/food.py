295352d7c70421b75898570c94570385fd74f3ca
solomon-asmr <selemonasmr7@gmail.com>
2025-09-16 21:47:35 +0300

    snake game final

---
+++ snake_game/food.py
@@ -0,0 +1,26 @@
+import random
+from turtle import Turtle
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.count=0
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
+
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
+
+    def bonus(self):
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=2, stretch_wid=2)
+        self.color("red")
+        self.speed("fastest")
+        self.refresh()
--- snake_game/main.py
+++ snake_game/main.py
@@ -8,6 +8,8 @@
 import time
 from turtle import Turtle, Screen
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 screen= Screen()
 screen.setup(600,600)
 screen.bgcolor("black")
@@ -15,6 +17,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food =Food()
+scoreboard = ScoreBoard()
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -26,21 +30,18 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    if snake.head.distance(food)<=15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()< -280:
+        game_is_on=False
+        scoreboard.game_over()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on=True
+            ScoreBoard.game_over()
 
 
 screen.exitonclick()
